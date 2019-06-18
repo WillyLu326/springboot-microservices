cdd738256ce9c2e8e1de158260b2f76cd4735183
Willy Lu <luzh0326@gmail.com>
2019-06-17 21:24:55 -0400

    created manager-ms

---
+++ manager-ms/src/main/java/com/microservice/manager/managerms/ManagerMsApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.manager.managerms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerMsApplication.class, args);
+	}
+
+}
