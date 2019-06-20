38ced1a7647d2fb375da532cd09fd29bdb01122e
Willy Lu <luzh0326@gmail.com>
2019-06-19 21:42:25 -0400

    created zuul mservice

---
+++ zull-ms/src/main/java/com/zuul/server/zullms/ZullMsApplication.java
@@ -0,0 +1,13 @@
+package com.zuul.server.zullms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZullMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZullMsApplication.class, args);
+	}
+
+}
